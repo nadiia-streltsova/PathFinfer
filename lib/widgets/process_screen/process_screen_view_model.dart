883bb5c7f7d53ea296f301a6a385ad4bc1f08af1
madiia-streltsova <lexy@email.ua>
2024-06-29 16:27:51 +0300

    loading animation

--- lib/widgets/process_screen/process_screen_view_model.dart
+++ lib/widgets/process_screen/process_screen_view_model.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:pathfinder/domain/api_client/api_client_exception.dart';
 import 'package:pathfinder/navigation/main_navigation.dart';
@@ -9,17 +11,22 @@ class ProcessScreenViewModel extends ChangeNotifier {
 
   String? get errorMessage => _errorMessage;
 
-  static const loadDataDelta = 50;
+  static const _loadDataDelta = 80;
 
   int _progressValue = 0;
+  int _animationLoadPercent= 0;
 
   int get progressValue => _progressValue;
 
-  bool get isCompleted => _progressValue >= 100;
+  bool _isSendingResults = false;
+  bool get isCompleted => _progressValue >= 100 && !_isSendingResults;
+
+  Timer? _timer;
 
   ProcessScreenViewModel(this.pathDataService);
 
   Future<void> startProcess(BuildContext context) async {
+    startTimer();
     try {
       await pathDataService.loadDataFromServer();
     } on ApiClientException catch (_) {
@@ -29,8 +36,6 @@ class ProcessScreenViewModel extends ChangeNotifier {
       }
       return;
     }
-    _progressValue += loadDataDelta;
-    notifyListeners();
 
     int tasksAmount = pathDataService.tasksAmount;
 
@@ -38,7 +43,7 @@ class ProcessScreenViewModel extends ChangeNotifier {
       _errorMessage = "Something went wrong, no tasks from server. Please, try again later.";
     }
 
-    int delta = ((100 - loadDataDelta) / tasksAmount).floor();
+    int delta = ((100 - _loadDataDelta) / tasksAmount).floor();
     for (int i = 0; i < tasksAmount; i++) {
       pathDataService.calcNextSolution(i);
       _progressValue += delta;
@@ -46,13 +51,39 @@ class ProcessScreenViewModel extends ChangeNotifier {
     }
   }
 
+  void startTimer() {
+    const duration = Duration(milliseconds: 100);
+    _timer = Timer.periodic(
+      duration,
+      (Timer timer) {
+        if (_loadDataDelta <= _animationLoadPercent) {
+          timer.cancel();
+        } else {
+          _animationLoadPercent++;
+          _progressValue++;
+          notifyListeners();
+        }
+      },
+    );
+  }
+
   Future<void> sendResultsToServer(BuildContext context) async {
+    _isSendingResults = true;
+    notifyListeners();
+
     _errorMessage = await pathDataService.validateData();
 
+    _isSendingResults = false;
     notifyListeners();
 
     if (_errorMessage == null && context.mounted) {
       Navigator.of(context).pushNamed(MainNavigationRouteNames.resultScreen);
     }
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
   }
 }
